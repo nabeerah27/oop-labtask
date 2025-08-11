a019e21248bca587cd34bf7a879e2ea9e2ef905f
Nabeerah Yaqoot <nabeerahyaqoot@gmail.com>
2025-08-12 03:26:57 +0500

    Uploaded Week 04 Lab Task

---
+++ src/W4_CuddlyToys/BankManager.java
@@ -0,0 +1,27 @@
+package W4_CuddlyToys;
+
+// BankManager class is a derived class (child) of BunnyRabbits
+public class BankManager extends BunnyRabbit {
+
+    private String job;
+
+    // Constructor - calls the parent (BunnyRabbits) constructor
+    // Sets a fixed color (White) and action (Manage accounts)
+
+    public BankManager(String size) {
+        super(size, "White");
+        this.job = "Manage accounts";
+    }
+
+    // Overriding the getRole method to specify this bunny's role
+    public String getJob() {
+        return job;
+    }
+
+    // Overriding toString() to include role and parent class details
+    @Override
+    public String toString() {
+        return "🏦 Bank manager [Size = " + getSize() + ", Color = " + getColor() + ", Job = " + job + "]";
+    }
+
+}
---
+++ src/W4_CuddlyToys/Main.java
@@ -0,0 +1,11 @@
+package W4_CuddlyToys;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ToyProgram toyProgram = new ToyProgram();
+        toyProgram.run();
+
+    }
+}
