f8093ada96c60f0592fa7b2a3022220572ef0368
Nabeerah Yaqoot <nabeerahyaqoot@gmail.com>
2025-08-18 14:51:34 +0500

    Update Main of vector

--- src/W7_Vector2D/code/Main.java
+++ src/W7_Vector2D/code/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Program program = new Program();
+        VectorApp program = new VectorApp();
         program.run();
 
     }
