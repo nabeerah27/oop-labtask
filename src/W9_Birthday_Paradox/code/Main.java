c13a76ec2e0249371dce8a914ef35b500901be7d
Nabeerah Yaqoot <nabeerahyaqoot@gmail.com>
2025-09-03 00:04:52 +0500

    Add code of Birthday Paradox

---
+++ src/W9_Birthday_Paradox/code/Main.java
@@ -0,0 +1,11 @@
+package W9_Birthday_Paradox.code;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Program program = new Program();
+        program.run();
+
+    }
+}
